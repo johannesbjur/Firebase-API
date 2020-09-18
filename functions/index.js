3068ce6ea4a5f4847b371e1290e23fd598546278
johannesbjur <bjurstromerjohannes@gmail.com>
2020-09-18 13:19:32 +0200

    get id function added + update id function added

--- functions/index.js
+++ functions/index.js
@@ -6,21 +6,31 @@ admin.initializeApp()
 
 const app = express();
 
-
+// GET Functions
 app.get('/', async (req, res) => {
     let snapshot = await admin.firestore().collection('products').get();
 
     let products = [];
-    snapshot.forEach( doc => {
+    snapshot.forEach( (doc) => {
         let id = doc.id;
         let data = doc.data();
 
-        products.push({id, ...data});
+        products.push({ id, ...data });
     });
 
     res.status(200).send(JSON.stringify(products));
 });
 
+app.get('/:id', async (req, res) => {
+    let snapshot = await admin.firestore().collection('products').doc(req.params.id).get();
+
+    let productId   = snapshot.id;
+    let productData = snapshot.data();
+
+    res.status(200).send(JSON.stringify({ id: productId, ...productData }));
+});
+
+// POST Functions
 app.post('/', async (req, res) => {
 
     let product = req.body;
@@ -28,6 +38,15 @@ app.post('/', async (req, res) => {
     await admin.firestore().collection('products').add(product);
 
     res.status(201).send();
+});
+
+// PUT Funtions
+app.put('/:id', async (req, res) => {
+    let body = req.body
+
+    await admin.firestore().collection('products').doc(req.params.id).update(body);
+
+    res.status(200).send();
 });
 
 exports.product = functions.https.onRequest(app);
