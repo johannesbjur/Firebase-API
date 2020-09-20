207ff2656896bb8efdbe1c2f5b35ac9a9038adae
johannesbjur <bjurstromerjohannes@gmail.com>
2020-09-20 16:30:04 +0200

    added create produts functionality to front end

--- web-app/scripts/main.js
+++ web-app/scripts/main.js
@@ -1,32 +1,48 @@
 const url = 'https://us-central1-fir-api-d8bf2.cloudfunctions.net'
-// const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-// const url = proxyUrl + apiUrl
 
-getAllProducts().then( data => {
+// addProduct("Orange", "fruit")
 
-    data.forEach( product => {
 
-        console.log(product)
-        document.getElementById('product-tbody').innerHTML = document.getElementById('product-tbody').innerHTML + 
-        `<tr>
-            <input type="hidden" value="${product.id}">
-            <th scope="row"></th>
-            <td>${product.name}</td>
-            <td>${product.type}</td>
-            <td><button type="button" class="btn btn-danger" onCLick="deleteClick(this.parentNode.parentNode)">Delete</button></td>
-         </tr>`
+function initializeApp() {
+
+    getAllProducts().then( data => {
+        data.forEach( product => {
+            document.getElementById('product-tbody').innerHTML = document.getElementById('product-tbody').innerHTML + 
+                `<tr>
+                    <input type="hidden" value="${product.id}">
+                    <th scope="row"></th>
+                    <td>${product.name}</td>
+                    <td>${product.type}</td>
+                    <td class="btn-td">
+                        <a href="#" class="btn btn-primary mr-3">Edit</a>
+                        <button type="button" class="btn btn-danger" onCLick="deleteClick(this.parentNode.parentNode)">Delete</button>
+                    </td>
+                    <td></td>
+                </tr>`
+        });
     });
-});
+}
+
+function addSubmit(form) {
+
+    let product = {
+        "name": form.getElementsByTagName("input")[0].value,
+        "type": form.getElementsByTagName("input")[1].value
+    }
+    addProduct(product).then( res => {
+        if(res) window.location.href = './index.html';
+    });
+}
 
 function deleteClick(item) {
 
     const id = item.querySelector("input").value
 
     deleteProduct(id);
-
     item.remove();
 }
 
+// API Functions
 async function getAllProducts() {
 
 	const response = await fetch(`${url}/product`, {
@@ -47,5 +63,21 @@ async function deleteProduct(id) {
     if(response.ok) {
         console.log("Item deleted")
     }
+}
 
+async function addProduct(product) {
+
+    const response = await fetch(`${url}/product`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(product)
+    });
+
+    if(response.ok) {
+        console.log("Item added")
+        return true
+    }
+    return false
 }
